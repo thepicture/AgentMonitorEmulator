c18f38e2e5e3a8e4f50cd9f55534b2813943cd13
thepicture <85333076+thepicture@users.noreply.github.com>
2022-08-31 19:43:55 +0500

    ci(PC): add client project

---
+++ AgentMonitorEmulator.AgentMonitor_PC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentMonitorEmulator.AgentMonitor_PC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
